4de22a96d5029643f9a91dde8947f9eecdb8b5e2
Kai <thiskaiguy@gmail.com>
2020-06-27 21:01:22 +0100

    Create a sketch of a new api

--- examples/basic.rs
+++ examples/basic.rs
@@ -1,5 +1,23 @@
 use stream_ui::*;
 
+struct Main {
+    is_true: bool,
+}
+impl Main {
+    view! {
+        Group {
+            title: "state",
+            margined: true,
+            child: Button {
+                text: self.is_true.to_string(),
+                #[on] clicked: Self::on_clicked,
+            },
+        }
+    }
+    fn on_clicked(&mut self) {
+        self.is_true = !self.is_true;
+    }
+}
 
 fn main() {
     App::new(|| {
